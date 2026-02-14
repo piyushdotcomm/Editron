3569943d09f4a45d3aab496a818f06b994311891
Piyush Kumar <13725shpiyush@gmail.com>
2026-02-14 10:13:07 +0530

    feat: Add a theme toggle component with Moon/Sun icons for switching between light and dark modes.

--- components/ui/toggle-theme.tsx
+++ components/ui/toggle-theme.tsx
@@ -2,31 +2,35 @@
 "use client";
 
 import { useTheme } from "next-themes";
-import { useEffect , useState } from "react";
+import { useEffect, useState } from "react";
 import { Moon, Sun, SunMoon } from "lucide-react";
 
 
-export function ThemeToggle(){
-    const {setTheme , theme} = useTheme();
-    const [mounted , setMounted] = useState(false);
+export function ThemeToggle() {
+    const { setTheme, theme, resolvedTheme } = useTheme();
+    const [mounted, setMounted] = useState(false);
 
     useEffect(() => {
         setMounted(true);
-    } , []);
+    }, []);
 
-    if(!mounted){
+    if (!mounted) {
         return null;
     }
 
     return (
-        <div 
-        className="cursor-pointer"
-        onClick={()=>{
-            setTheme(theme === "light" ? "dark" : "light");
-        }}
+        <div
+            className="cursor-pointer"
+            onClick={() => {
+                setTheme(resolvedTheme === "light" ? "dark" : "light");
+            }}
         >
             {
-                theme === "light" ? (<Moon className="h-5 w-5 text-black"/>) : (<Sun className="h-5 w-5 text-white" color="white"/>)
+                resolvedTheme === "light" ? (
+                    <Moon className="h-5 w-5 text-foreground hover:text-primary transition-colors" />
+                ) : (
+                    <Sun className="h-5 w-5 text-foreground hover:text-primary transition-colors" />
+                )
             }
         </div>
     )
