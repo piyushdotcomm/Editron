1b261221671f4ffb81cd9a577069f79f1f4284e8
ojasdhargave-iiitv <202451120@iiitvadodara.ac.in>
2026-05-12 23:55:49 +0530

    fix: add accessible focus-visible styles

--- modules/dashboard/components/template-selecting-modal.tsx
+++ modules/dashboard/components/template-selecting-modal.tsx
@@ -152,7 +152,7 @@ const TemplateSelectionModal = ({
               <div className="flex flex-col sm:flex-row gap-4">
                 <div className="relative flex-1">
                   <Search
-                    className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 outline-none"
+                    className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 outline-none focus-visible:ring-1 focus-visible:ring-primary"
                     size={18}
                   />
                   <Input
