74dc2e8994d4591bfcd3ee7663e06fb876505be8
Sanjay Narayanan <sanjay.narayanan3010@gmail.com>
2026-05-13 22:27:08 +0530

    fix: add local type declaration for y-websocket (#27)

---
+++ types/y-websocket.d.ts
@@ -0,0 +1,30 @@
+declare module 'y-websocket' {
+  import * as Y from 'yjs'
+
+  export class WebsocketProvider {
+    constructor(
+      serverUrl: string,
+      roomname: string,
+      doc: Y.Doc,
+      options?: {
+        connect?: boolean
+        awareness?: object
+        params?: Record<string, string>
+        WebSocketPolyfill?: typeof WebSocket
+        resyncInterval?: number
+        maxBackoffTime?: number
+        disableBc?: boolean
+      }
+    )
+    wsconnected: boolean
+    wsconnecting: boolean
+    bcconnected: boolean
+    shouldConnect: boolean
+    awareness: object
+    connect(): void
+    disconnect(): void
+    destroy(): void
+    on(event: string, handler: (...args: unknown[]) => void): void
+    off(event: string, handler: (...args: unknown[]) => void): void
+  }
+}
