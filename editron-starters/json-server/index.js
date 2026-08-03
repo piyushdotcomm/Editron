97958249adbf943309b1b8c77fbfaf41d8650da9
Saurabh Kumar Bajpai <saurabhkumarbajpaiai@gmail.com>
2026-08-03 15:14:57 +0530

    fix: resolve 2 bugs in Editron

--- editron-starters/json-server/index.js
+++ editron-starters/json-server/index.js
@@ -4,7 +4,7 @@ fetch(url)
   .then((r) => r.json())
   .then(
     (json) =>
-      (document.getElementById('output').innerHTML = JSON.stringify(
+      (document.getElementById('output').textContent = JSON.stringify(
         json,
         null,
         2
